aa164d51748076952ae99cc1bcecdd8663acdf6a
viktor <v.dome1991@gmail.com>
2021-04-06 22:00:14 +0200

    [add] basic snake placement and minor board change

--- snake.js
+++ snake.js
@@ -1,15 +1,18 @@
 const game = {
 
     createBoard: function(){
-        let board = document.querySelector('.game-container')
+        let board = document.querySelector('.game-container');
+        this.createHeader(board);
+        this.createGameBoard(board);
+        let gameField = document.querySelector('.game-board');
+
         for (let row_idx = 0; row_idx < 10; row_idx++){
-            if (row_idx === 0)
-                this.createHeader(board);
-            const newRow = game.addRow(board);
+            const newRow = game.addRow(gameField);
             for(let col_idx = 0; col_idx < 18; col_idx++){
                 this.addCol(newRow, row_idx, col_idx);
             }
         }
+        this.placeSnakeOnBoard()
     },
     addRow: function (board) {
         board.insertAdjacentHTML(
@@ -25,11 +28,26 @@ const game = {
                         data-row="${row_idx}"
                         data-col="${col_idx}">.</div>`);
     },
-    createHeader: function (board){
+    createHeader: function (board) {
         board.insertAdjacentHTML(
             'beforeend',
             '<div class="board-header">This is the header</div>'
         );
+    },
+    createGameBoard: function (board){
+        board.insertAdjacentHTML(
+            'beforeend',
+            '<div class="game-board"></div>'
+        );
+    },
+    placeSnakeOnBoard: function (){
+        let snakePart0 = document.querySelector('[data-row="4"][data-col="4"]');
+        let snakePart1 = document.querySelector('[data-row="4"][data-col="5"]');
+        let snakePart2 = document.querySelector('[data-row="4"][data-col="6"]');
+
+        for (let i = 7; i < 10; i++){
+            document.querySelector(`[data-row="4"][data-col="${i}"]`).style.backgroundColor = "yellow";
+        }
     }
 }
 game.createBoard()
