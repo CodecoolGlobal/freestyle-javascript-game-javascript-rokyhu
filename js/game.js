3bca8d64954e25a6f0e647bf2c72aa3c8d90d267
Marta Kowalska <martaaa.kowalska@gmail.com>
2021-04-07 12:13:07 +0200

    [fix] moveInvaders function

--- js/game.js
+++ js/game.js
@@ -1,37 +1,6 @@
 const game = {
     rows : 12,
     cols : 18,
-    // sprite: {
-    //         '189': 'player',
-    //         '20':"first_enemy",
-    //         '22':"first_enemy",
-    //         '24':"first_enemy",
-    //         '26':"first_enemy",
-    //         '28':"first_enemy",
-    //         '30':"first_enemy",
-    //         '32':"first_enemy",
-    //         '39':"second_enemy",
-    //         '41':"second_enemy",
-    //         '43':"second_enemy",
-    //         '45':"second_enemy",
-    //         '47':"second_enemy",
-    //         '49':"second_enemy",
-    //         '51':"second_enemy",
-    //         '56':"third_enemy",
-    //         '58':"third_enemy",
-    //         '60':"third_enemy",
-    //         '62':"third_enemy",
-    //         '64':"third_enemy",
-    //         '66':"third_enemy",
-    //         '68':"third_enemy",
-    //         '75':"fourth_enemy",
-    //         '77':"fourth_enemy",
-    //         '79':"fourth_enemy",
-    //         '81':"fourth_enemy",
-    //         '83':"fourth_enemy",
-    //         '85':"fourth_enemy",
-    //         '87':"fourth_enemy",
-    //     },
 
     init: function () {
         this.drawBoard();
@@ -50,28 +19,27 @@ const game = {
             '28':"first_enemy",
             '30':"first_enemy",
             '32':"first_enemy",
-            // '39':"second_enemy",
-            // '41':"second_enemy",
-            // '43':"second_enemy",
-            // '45':"second_enemy",
-            // '47':"second_enemy",
-            // '49':"second_enemy",
-            // '51':"second_enemy",
-            // '56':"third_enemy",
-            // '58':"third_enemy",
-            // '60':"third_enemy",
-            // '62':"third_enemy",
-            // '64':"third_enemy",
-            // '66':"third_enemy",
-            // '68':"third_enemy",
-            // '75':"fourth_enemy",
-            // '77':"fourth_enemy",
-            // '79':"fourth_enemy",
-            // '81':"fourth_enemy",
-            // '83':"fourth_enemy",
-            // '85':"fourth_enemy",
-            // '87':"fourth_enemy",
-
+            '39':"second_enemy",
+            '41':"second_enemy",
+            '43':"second_enemy",
+            '45':"second_enemy",
+            '47':"second_enemy",
+            '49':"second_enemy",
+            '51':"second_enemy",
+            '56':"third_enemy",
+            '58':"third_enemy",
+            '60':"third_enemy",
+            '62':"third_enemy",
+            '64':"third_enemy",
+            '66':"third_enemy",
+            '68':"third_enemy",
+            '75':"fourth_enemy",
+            '77':"fourth_enemy",
+            '79':"fourth_enemy",
+            '81':"fourth_enemy",
+            '83':"fourth_enemy",
+            '85':"fourth_enemy",
+            '87':"fourth_enemy",
         }
         for (let row = 0; row < this.rows; row++) {
             const rowElement = this.addRow(gameField);
@@ -127,17 +95,11 @@ const game = {
     moveInvaders: function () {
         let direction = "left";
         setInterval(function () {
-        //     console.log()
-        // }////
-            let enemyDivs = document.querySelectorAll(".first_enemy")
-
+            let enemyDivs = document.querySelectorAll('[class*=enemy]')
         let enemyColumns = [...enemyDivs].map(item => {
             return item.dataset.col; })
         for (let index = 0; index < enemyDivs.length; index++) {
-            // console.log(enemyDivs[index].classList.contains('first_enemy'))
-            let enemyClass = "first_enemy"
-            // console.log(enemyColumns)
-
+            let enemyClass = enemyDivs[index].getAttribute('class').split(" ")[1];
             if (direction === "left" && !enemyColumns.includes('0')) {
                 enemyDivs[index].previousSibling.classList.add(enemyClass);
                 enemyDivs[index].classList.remove(enemyClass);
@@ -148,36 +110,24 @@ const game = {
             }
             if (enemyColumns.includes('0') && direction === "left") {
                 direction = "right";
-                // enemyDivs[index].nextSibling.classList.add(enemyClass);
-                // enemyDivs[index].classList.remove(enemyClass);
-                for (let newIndex = 0; newIndex < enemyDivs.length; newIndex++) {
+                for (let newIndex = 0; newIndex <= enemyDivs.length; newIndex++) {
+                    let enemyClass = enemyDivs[newIndex].getAttribute('class').split(" ")[1];
                     let i = enemyDivs[newIndex].getAttribute('data-col');
                     let j = enemyDivs[newIndex].getAttribute('data-row');
-                //     console.log(enemyDivs[newIndex])
-                    // enemyDivs[newIndex].classList.remove(enemyC);
                     let lower = document.querySelector(`.field[data-col="${i}"][data-row="${parseInt(j) + 1}"]`)
-                    lower.classList.add(enemyClass);
                     enemyDivs[newIndex].classList.remove(enemyClass);
-                    console.log(enemyDivs[newIndex].className)
-                }
-
-                //
-                // let actualShootPos = document.querySelector(.field[data-col="${j}"][data-row="${i}"]);
-                // let prevShootPos = document.querySelector(.field[data-col="${j}"][data-row="${i+1}"]);
-                }
-
+                    lower.classList.add(enemyClass);
+                }}
              if (enemyColumns.includes('17') && direction === "right") {
                  direction = "left";
-                 enemyDivs[index].previousSibling.classList.add(enemyClass);
-                 enemyDivs[index].classList.remove(enemyClass);
-                 // for (let newIndex = 0; newIndex < enemyDivs.length; newIndex++) {
-                 //     let enemyC1 = "first_enemy"
-                 //     let i = enemyDivs[newIndex].getAttribute('data-col');
-                 //     let j = enemyDivs[newIndex].getAttribute('data-row');
-                 // enemyDivs[newIndex].className = 'field'
-                 // let lower = document.querySelector(`[data-col="${i}"][data-row="${parseInt(j) + 1}"]`)
-                 // lower.classList.add(enemyC1);
-                 // }}
+                 for (let newIndex = 0; newIndex <= enemyDivs.length; newIndex++) {
+                     let enemyClass = enemyDivs[newIndex].getAttribute('class').split(" ")[1];
+                     let i = enemyDivs[newIndex].getAttribute('data-col');
+                     let j = enemyDivs[newIndex].getAttribute('data-row');
+                 let lower = document.querySelector(`.field[data-col="${i}"][data-row="${parseInt(j) + 1}"]`)
+                 enemyDivs[newIndex].classList.remove(enemyClass);
+                 lower.classList.add(enemyClass);
+                 }
              }
 
         }}, 1500)
@@ -185,20 +135,7 @@ const game = {
     },
 
 
-    // moveInvaders2: function () {
-    //     let direction = "left";
-    //     setInterval(function () {
-    //         let size=0;
-    //             for(let k in game.sprite) {
-    //               size++
-    //                 console.log(game.sprite[k])
-    //             }
-    //         for (let index = 0; index < size; index++) {
-    //             let indexStr =  index.toString();
-    //         }
-    //     }, 500)
-    //
-    // }
+
 }
 
 game.init();
